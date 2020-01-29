aa90e464de5d3eaabb7d54874719292566a0d9dd
Xabi Ezpeleta <xezpeleta@gmail.com>
2020-01-29 12:53:15 +0100

    Check/create db directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+echo "***********************************"
+echo "********** ELKARFORTI *************"
+echo " DB_PATH: $DB_PATH"
+echo "***********************************"
+
+if [ ! -d "dirname $DB_PATH" ]
+then
+  mkdir "$(dirname $DB_PATH)"
+fi
+
 cd /data/web
 python3 manage.py migrate --noinput
 python3 manage.py runserver 0.0.0.0:8000
